138dcaf8b66faba98ed9fe369e2c5daa040cad15
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2022-10-05 20:11:16 +0200

    added features

--- client/src/__tests__/FileInput.component.test.js
+++ client/src/__tests__/FileInput.component.test.js
@@ -1,39 +1,33 @@
 import FileInput from '../components/FileInput.component';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import fakeImg from './mocks/mocks';
 import { Simulate } from 'react-dom/test-utils';
 
 let inputFile;
 let figure;
 let setImageUpload = jest.fn();
-let selectedFile = false; 
-beforeEach(() => {
 
-  render(<FileInput setImageUpload={setImageUpload}/>);
-  inputFile = screen.getByTestId('image-input');
-  figure = screen.queryByRole('figure');
-});
+describe('Tests of FileInput component', () => {
+  beforeEach(() => {
+    render(<FileInput setImageUpload={setImageUpload} />);
+    inputFile = screen.getByTestId('image-input');
+    figure = screen.queryByRole('figure');
+  });
 
-test('should show input file box', () => {
-  expect(inputFile).toBeInTheDocument();
-});
+  test('should show input file box', () => {
+    expect(inputFile).toBeInTheDocument();
+  });
 
-test('figure not rendering at initial state', () => {
-  expect(figure).toBeNull();
-});
+  test('figure not rendering at initial state', () => {
+    expect(figure).toBeNull();
+  });
 
-test('user can upload a file', async () => {
-  Simulate.change(inputFile, {target: { files: [fakeImg]}});
-  // Once user uploads the onSelectFile functions gets triggered
-  setImageUpload.mockReturnValueOnce(!selectedFile)
-  expect(setImageUpload).toHaveBeenCalledTimes(1);
-  figure = screen.getByRole('figure');
-  expect(figure).toBeInTheDocument()
+  test('user can upload a file', async () => {
+    Simulate.change(inputFile, { target: { files: [fakeImg] } });
+    // Once user uploads the onSelectFile functions from FileInputComponent gets triggered
+    expect(setImageUpload).toHaveBeenCalledTimes(1);
+  });
 })
-
-
-
 
 
 
--- client/src/components/LocationInput.component.js
+++ client/src/components/LocationInput.component.js
@@ -5,14 +5,46 @@ import 'mapbox-gl-style-switcher/styles.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { createInputMap } from '../utils/maps';
 import { draggableMarker } from '../utils/helperFuncs';
+import { initializeMap, mapSearchBar, locationControl } from '../utils/helperFuncs';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl-style-switcher/styles.css';
+import { MapboxStyleSwitcherControl } from 'mapbox-gl-style-switcher';
+import { switcherStyles } from '../utils/switcherStyles';
+
 
 const LocationInput = ({ currentLocation, setCoordinates }) => {
   const mapContainer = useRef(null);
   const map = useRef(null);
+  // const [searchb, setGeocoder]
 
+   
   useEffect(() => {
-    createInputMap(map, mapContainer, currentLocation);
+    map.current = initializeMap(mapContainer, currentLocation);
 
+    const geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl,
+      marker: false,
+    });
+    map.current.addControl(geocoder);
+    // go to user's location control
+    const userLocation = locationControl()
+    map.current.addControl(userLocation);
+    //   // add style switcher
+    map.current.addControl(new MapboxStyleSwitcherControl(switcherStyles));
+    //   // add zoom and rotation controls to the map.
+    map.current.addControl(new mapboxgl.NavigationControl());
+   
+    // console.log('this is geocoder.current', geocoder);
+    geocoder.on('result', (e) => {
+      marker.setLngLat(e.result.center);
+    })
+
+    userLocation.on('geolocate', (e) => {
+      console.log('hellooo', e);
+      setTimeout(marker.setLngLat([e.coords.longitude, e.coords.latitude], 5))
+    })
     // define properties of draggable marker
     let pin = document.createElement('div');
     pin.className = 'marker';
@@ -24,7 +56,6 @@ const LocationInput = ({ currentLocation, setCoordinates }) => {
       pin,
       map.current
     );
-
     // get the coordinates from the position where the pin was dragged to
     const getCoordinates = () => {
       const coordinates = marker.getLngLat();
@@ -36,7 +67,7 @@ const LocationInput = ({ currentLocation, setCoordinates }) => {
   }, [setCoordinates]);
 
   return (
-    <div className='set-location-map-container'>
+    <div data-testid='map-container' className='set-location-map-container'>
       <div
         ref={mapContainer}
         className='add-location-map'
@@ -46,4 +77,12 @@ const LocationInput = ({ currentLocation, setCoordinates }) => {
 };
 
 export default LocationInput;
+
+
+
+
+
+
+
+
 
